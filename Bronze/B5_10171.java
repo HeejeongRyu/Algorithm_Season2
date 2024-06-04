cabbefb83b17d4f64b77fd02c41983839900feee
HeejeongRyu <95456557+HeejeongRyu@users.noreply.github.com>
2024-06-04 21:54:50 +0900

    Create B5_10171.java

---
+++ Bronze/B5_10171.java
@@ -0,0 +1,14 @@
+/*아래 예제와 같이 고양이를 출력하시오.*/
+package stap_1;
+
+public class B5_10171 {
+ 
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");   
+ 
+	}
+}
