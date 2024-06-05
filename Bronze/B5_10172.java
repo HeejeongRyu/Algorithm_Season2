f50238a919ec0126eeea9130c5b11384510b074a
HeejeongRyu <95456557+HeejeongRyu@users.noreply.github.com>
2024-06-05 22:21:07 +0900

    Create B5_10172.java

---
+++ Bronze/B5_10172.java
@@ -0,0 +1,15 @@
+/*아래 예제와 같이 개를 출력하시오.*/
+package stap_1;
+
+public class B5_10172 {
+ 
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");  
+ 
+	}
+}
